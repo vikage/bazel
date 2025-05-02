5665fb0a187dee87ff2e5d9f50652f196cba9183
Thanh Vu <thanh@luongthien.com>
2025-05-02 14:48:38 +0700

    Show error when modified inputs during execution

--- src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
@@ -99,6 +99,7 @@ import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.util.io.TimestampGranularityMonitor;
 import com.google.devtools.build.lib.vfs.FileSystem;
+import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.Root;
 import com.google.devtools.build.skyframe.SkyFunction;
@@ -108,6 +109,7 @@ import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
 import com.google.devtools.build.skyframe.SkyframeLookupResult;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -904,6 +906,9 @@ public final class ActionExecutionFunction implements SkyFunction {
         }
       }
       checkState(!env.valuesMissing(), action);
+
+      checkForConcurrentModifications(action, inputMetadataProvider);
+
       skyframeActionExecutor.updateActionCache(
           action,
           inputMetadataProvider,
@@ -916,6 +921,38 @@ public final class ActionExecutionFunction implements SkyFunction {
               expandedFilesets),
           state.token,
           clientEnv);
+    }
+
+    private void checkForConcurrentModifications(Action action, InputMetadataProvider inputMetadataProvider) throws ActionExecutionException {
+      Path execRoot = skyframeActionExecutor.getExecRoot();
+
+      for (ActionInput input : action.getInputs().toList()) {
+        try {
+          if (!(input instanceof Artifact artifact
+              && artifact.isSourceArtifact()
+              && !artifact.getRoot().isExternal())) {
+            continue;
+          }
+
+          FileArtifactValue metadata = inputMetadataProvider.getInputMetadata(input);
+
+          Path path = execRoot.getRelative(input.getExecPath());
+          if (metadata.wasModifiedSinceDigest(path)) {
+            String errorMessage = String.format("input %s was modified during execution of action", input.getExecPath());
+            skyframeActionExecutor.printError(errorMessage, action);
+
+            for (Artifact output: action.getOutputs()) {
+              Path outputPath = execRoot.getRelative(output.getExecPath());
+              outputPath.delete();
+            }
+
+            throw new ActionExecutionException(errorMessage, action, false, createDetailedExitCodeForInputModified(errorMessage));
+          }
+        }
+        catch (IOException e) {
+          logger.atWarning().log("Failed to get check modification for input %s: %s", input, e.getMessage());
+        }
+      }
     }
   }
 
@@ -1782,5 +1819,13 @@ public final class ActionExecutionFunction implements SkyFunction {
             .setMessage(message)
             .setExecution(Execution.newBuilder().setCode(Code.DISCOVERED_INPUT_DOES_NOT_EXIST))
             .build());
+  }
+
+  private static DetailedExitCode createDetailedExitCodeForInputModified(String message) {
+    return DetailedExitCode.of(
+            FailureDetail.newBuilder()
+                    .setMessage(message)
+                    .setExecution(Execution.newBuilder().setCode(Code.INPUT_FILE_MODIFIED_DURING_EXECUTION))
+                    .build());
   }
 }
